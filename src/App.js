ce649ca1afd9df42edfa08daad6e21cd251b5e89
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-27 15:29:36 +0600

    🛠️completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import Left from "./Components/Left";
+import Right from "./Components/Right";
 
-function App() {
+export default function App() {
+  const [CardNumber, setCardNumber] = useState();
+  const [Name, setName] = useState();
+  const [ExpMonth, setExpMonth] = useState();
+  const [ExpYear, setExpYear] = useState();
+  const [Cvc, setCvc] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Left
+        CardNumber={CardNumber}
+        Name={Name}
+        ExpMonth={ExpMonth}
+        ExpYear={ExpYear}
+        cvc={Cvc}
+      />
+      <Right
+        CardNumber={CardNumber}
+        Name={Name}
+        ExpMonth={ExpMonth}
+        ExpYear={ExpYear}
+        cvc={Cvc}
+        setCardNumber={setCardNumber}
+        setName={setName}
+        setExpMonth={setExpMonth}
+        setExpYear={setExpYear}
+        setCvc={setCvc}
+      />
+    </Container>
   );
 }
 
-export default App;
+const Container = styled.main`
+  max-width: 1440px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  @media (max-width: 600) {
+    grid-template-columns: 1fr;
+  }
+  @media (min-width: 900px) {
+    height: min(100vh, 900px);
+  }
+`;
